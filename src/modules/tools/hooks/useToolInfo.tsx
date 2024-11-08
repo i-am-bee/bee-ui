126b1acf5dde427f09f664048a5c0dba149de4c4
Petr Kadlec <petr@puradesign.cz>
2024-11-08 14:02:30 +0100

    fixup! add copyright
    Signed-off-by: Petr Kadlec <petr@puradesign.cz>

--- src/modules/tools/hooks/useToolInfo.tsx
+++ src/modules/tools/hooks/useToolInfo.tsx
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2024 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { useQuery } from '@tanstack/react-query';
 import { useAppContext } from '@/layout/providers/AppProvider';
 import { ToolReference } from '@/app/api/tools/types';
@@ -16,7 +32,7 @@ import {
   SearchLocate,
   Tools,
 } from '@carbon/react/icons';
-import { SkeletonIcon, SkeletonPlaceholder, SkeletonText } from '@carbon/react';
+import { SkeletonIcon } from '@carbon/react';
 import { ToolNameSkeleton } from '../components/ToolNameSkeleton';
 
 export function useToolInfo(toolReference: ToolReference) {
