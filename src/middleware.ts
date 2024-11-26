c767cd394646e6a7f0c2ce46391657366b8e48cc
Jan Dušek <jan@dusek.xyz>
2024-11-26 14:22:19 +0100

    fix: csp in dev mode
    Signed-off-by: Jan Dušek <jan@dusek.xyz>

--- src/middleware.ts
+++ src/middleware.ts
@@ -85,7 +85,9 @@ export const config = {
 const getCspHeader = (nonce: string) => {
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${
+      process.env.NODE_ENV === 'production' ? '' : `'unsafe-eval'`
+    };
     style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data: www.ibm.com/;
     font-src 'self';
